0d782afc44946240411b0cdc5d402dee64d4f5b5
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-02-15 12:57:03 -0500

    add acorddion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
-import { Box } from '@mui/material'
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Table from '../components/layouts/table'
+import Table from '../components/layouts/table';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import styles from '../styles/Home.module.css'
 
 // import data
 import data from '../data.json';
 
 const Home: NextPage = () => {
+  const accordionSummaryStyle = {
+    backgroundColor: 'gray',
+  }
+
+  const tableStyles = {
+    width: 1000,
+    height: 500,
+  }
   
   return (
     <>
@@ -15,8 +24,52 @@ const Home: NextPage = () => {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box sx={{width: 1000, height: 500}}> 
-        <Table data={data} />
+      <Box sx={{width: '100%'}}>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            style={accordionSummaryStyle}
+          >
+            <Typography>Impuestos Nacionales</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box sx={tableStyles}>
+              <Table data={data} />
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            style={accordionSummaryStyle}
+          >
+            <Typography>Impuestos Departamentales</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box sx={tableStyles}>
+              <Table data={data} />
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            style={accordionSummaryStyle}
+          >
+            <Typography>Impuestos Municipales</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box sx={tableStyles}>
+              <Table data={data} />
+            </Box>
+          </AccordionDetails>
+        </Accordion>
       </Box>
     </>
   )

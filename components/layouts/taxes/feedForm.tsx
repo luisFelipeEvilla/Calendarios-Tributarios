ce4b5711398915b77874b9e7413625c198b0403a
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-02-21 19:16:50 -0500

    add feed presentation day

---
+++ components/layouts/taxes/feedForm.tsx
@@ -0,0 +1,84 @@
+import { Box, Button, Modal, TextField, Typography } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { useState } from "react";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from "dayjs";
+
+
+export default function FeedForm({ ...props }) {
+    const [modal, setModal] = useState(false);
+    const [nit, setNit] = useState(0);
+    const [date, setDate] = useState<Dayjs | null>(
+        dayjs(new Date()),
+    );
+
+    const handleDateChange = (newDate: Dayjs | null) => {
+        setDate(newDate);
+    };
+
+    const handleSubmit = () => {
+        props.handleAddFeed(props.index, { nit, date: date })
+        setModal(false);
+    }
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '1px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
+    return (
+        <Box>
+            <Button onClick={e => setModal(true)} variant="contained" color="success">Agregar</Button>
+            <Modal
+                open={modal}
+                onClose={e => setModal(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography variant="h4" component="h1" marginBottom={8} textAlign='center'>
+                        Agregar Fecha de presentación
+                    </Typography>
+
+                    <FormGroup>
+                        <FormControl>
+                            <TextField
+                                id="nit"
+                                label="Digito/s de asignación"
+                                type="number"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                onChange={(e) => setNit(parseInt(e.target.value))}
+                            />
+                        </FormControl>
+                        <FormControl sx={{ marginTop: 3 }}>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DesktopDatePicker
+                                    label="Fecha de presentación"
+                                    inputFormat="MM/DD/YYYY"
+                                    value={date}
+                                    onChange={handleDateChange}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                        </FormControl>
+                    </FormGroup>
+                    <Box display='flex' justifyContent='flex-end' marginTop={3}>
+                        <Button sx={{marginRight: 2}} onClick={e => setModal(false)} variant="contained" color="error">Cancelar</Button>
+                        <Button onClick={handleSubmit} variant="contained" color="success">Guardar</Button>
+                    </Box>
+                </Box>
+            </Modal >
+        </Box >
+    )
+}
--- pages/calendarioTributario/create.tsx
+++ pages/calendarioTributario/create.tsx
@@ -1,51 +1,27 @@
 import AccountBalance from "@mui/icons-material/AccountBalance";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Accordion, AccordionDetails, AccordionSummary, Avatar, Box, Button, FormGroup, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Avatar, Box, Button, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
 import { SelectChangeEvent } from "@mui/material/Select/SelectInput";
-import { DatePicker, DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import Layout from "../../components/layout";
 import TaxesScheduler from "../../components/layouts/schedulers/taxesScheduler";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import FeedForm from "../../components/layouts/taxes/feedForm";
+
+type Feed = {nit: number, date: Date}
 
 export default function Create() {
     const avatarSize = { width: 120, height: 120 }
     const avatarIconSize = { width: 80, height: 80 }
     const [name, setName] = useState('');
     const [period, setPeriod] = useState(1);
-    const [installments, setInstallments] = useState([[{ nit: 1, fecha: new Date() }, { nit: 2, fecha: new Date() }]]);
-    
+    const [feeds, setFeeds] = useState([[{ nit: 1, date: new Date() }, { nit: 2, date: new Date() }]]);
+
+    // hooks
     const router = useRouter();
 
-    const handlePeriodChange = (event: SelectChangeEvent<unknown>) => {
-        const periodsNumber = event.target.value as number;
-        setPeriod(periodsNumber);
-        
-        const frequency = periodos.find(periodo => periodo.value === periodsNumber)?.frequency || 0;
-        
-        const newInstallments = [];
-
-        for (let i = 0; i < frequency; i++) {
-            newInstallments.push([]);
-        }
-
-        // @ts-ignore
-        setInstallments(newInstallments);
-    }
-
-    const handleSubmit = () => {
-        //Todo implement save tax logic
-        router.push('/calendarioTributario')
-    }
-
-    const handleAddInstallment = (index: number) => {
-        const newInstallments = [...installments];
-        newInstallments[index].push({ nit: 1, fecha: new Date() });
-        setInstallments(newInstallments);
-    }
-
+    // fake data
     const periodos = [
         { name: 'Anual', value: 1, frequency: 1 },
         { name: 'Semestral', value: 2, frequency: 2 },
@@ -56,64 +32,93 @@ export default function Create() {
         { name: 'Personalizado', value: 7, frequency: 1 }
     ]
 
+    // styles
     const accordionSummaryStyle = {
         backgroundColor: "primary.main",
         color: 'white',
     }
 
-    const getInstallments = () => {
-        const components = [];
+    // functions
+    const handleSubmit = () => {
+        //Todo implement save tax logic
+        router.push('/calendarioTributario')
+    }
+
+    const handleAddFeed = (index: number, feed: Feed) => {
+        //@ts-ignore
+        const newFeed = { nit: feed.nit, date: feed.date['$d'] }
+        const newFeeds = [...feeds];
+        newFeeds[index].push(newFeed);
+       
+        setFeeds(newFeeds);        
+    }
+
+    const handlePeriodChange = (event: SelectChangeEvent<unknown>) => {
+        const periodsNumber = event.target.value as number;
+        setPeriod(periodsNumber);
+
+        const frequency = periodos.find(periodo => periodo.value === periodsNumber)?.frequency || 0;
+
+        const newFeeds = [];
+
+        for (let i = 0; i < frequency; i++) {
+            newFeeds.push([]);
+        }
+
+        // @ts-ignore
+        setFeeds(newFeeds);
+    }
+
+
+    const getFeeds = () => {
         const frequency = periodos.find((periodo) => periodo.value === period)?.frequency || 0;
 
         const initDate = new Date(new Date().getFullYear(), 0, 1);
         const endDate = new Date(new Date().getFullYear(), 12 / frequency, 0);
 
-        for (let i = 0; i < frequency; i++) {
-            const installment = installments[i];
-
-            components.push(
-                <Accordion sx={{ width: '100%' }} key={i}>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                        sx={accordionSummaryStyle}
-                    >
-                        <Typography>Cuota No. {i + 1}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <p><b>Periodo:</b> {initDate.toDateString()} - {endDate.toDateString()}</p>
-                        <Button onClick={e => handleAddInstallment(i)} variant="contained" color="success">Agregar</Button>
-                        <TableContainer component={Paper}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Digito/s de asignación</TableCell>
-                                        <TableCell>Fecha de presentación</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {
-                                        installment.map((installment) => (
-                                            <TableRow>
-                                                <TableCell>{installment.nit}</TableCell>
-                                                <TableCell>{installment.fecha.toDateString()}</TableCell>
-                                            </TableRow>
-                                        ))
-                                    }
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </AccordionDetails>
-                </Accordion>
-            )
+        const components = feeds.map((installment, index) => {
+            const component = <Accordion sx={{ width: '100%' }} key={index}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}
+                    sx={accordionSummaryStyle}
+                >
+                    <Typography>Cuota No. {index + 1}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <p><b>Periodo:</b> {initDate.toDateString()} - {endDate.toDateString()}</p>     
+                    <FeedForm index={index} handleAddFeed={handleAddFeed}/>               
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Digito/s de asignación</TableCell>
+                                    <TableCell>Fecha de presentación</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    installment.map((installment) => (
+                                        <TableRow>
+                                            <TableCell>{installment.nit}</TableCell>
+                                            <TableCell>{installment.date.toDateString()}</TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </AccordionDetails>
+            </Accordion>
 
             initDate.setMonth(initDate.getMonth() + 12 / frequency);
             endDate.setMonth(endDate.getMonth() + 12 / frequency + 1, 0);
-        }
+
+            return component;
+        })
 
         return components;
     }
+
     return (
         <Layout>
             <Box display={'flex'} alignItems='center' flexDirection={'column'}>
@@ -140,7 +145,7 @@ export default function Create() {
                             autoFocus
                         />
                     </FormControl>
-                    <FormControl fullWidth >
+                    <FormControl fullWidth>
                         <InputLabel> Frecuencia</InputLabel>
                         <Select name='Periodicidad' value={period} fullWidth label='frecuencia' onChange={handlePeriodChange}>
                             {
@@ -157,7 +162,7 @@ export default function Create() {
                     <Box width='80%' marginBottom={10}>
                         <TaxesScheduler />
                     </Box>
-                    {getInstallments()}
+                    {getFeeds()}
                 </Box>
 
                 <Box display='flex' justifyContent='center' width='100%' marginTop={5} marginBottom={5}>

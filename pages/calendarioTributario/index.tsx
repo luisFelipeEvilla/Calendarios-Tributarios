2ba56e9c3eb4c7ecee182c872f7447089d18273f
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-03-01 16:32:35 -0500

    calendarios tributarios with client side rendering

--- pages/calendarioTributario/index.tsx
+++ pages/calendarioTributario/index.tsx
@@ -6,15 +6,40 @@ import Head from 'next/head';
 import Layout from '../../components/layout';
 import Accordeon from '../../components/layouts/accordion';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Spinner from '../../components/layouts/spinner';
 
 
 const Home: NextPage = ({ ...props }: any) => {
-  // @ts-ignore
-  const nacionales = props.taxes.filter((tax) => tax.tipo == 1);
-  // @ts-ignore
-  const departamentales = props.taxes.filter((tax) => tax.tipo == 2);
-  // @ts-ignore
-  const municipales = props.taxes.filter((tax) => tax.tipo == 3);
+  const [nacionales, setNacionales] = useState([]);
+  const [departamentales, setDepartamentales] = useState([]);
+  const [municipales, setMunicipales] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/impuesto`;
+
+    let taxes = [];
+    try {
+      const request = await axios.get(url);
+      const taxes = request.data.data;
+      // @ts-ignore
+      setNacionales(taxes.filter((tax) => tax.tipo == 1));
+      // @ts-ignore
+      setDepartamentales(taxes.filter((tax) => tax.tipo == 2));
+      // @ts-ignore
+      setMunicipales(taxes.filter((tax) => tax.tipo == 3));
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <Layout>
@@ -27,37 +52,24 @@ const Home: NextPage = ({ ...props }: any) => {
           <Typography variant="h2" component="h1" gutterBottom> Calendario Tributario </Typography>
           <Typography variant='body1'>Aqui podras Encontrar todar la información relacionada a los calendarios tributarios</Typography>
         </Box>
-        <Box sx={{ width: '100%' }}>
-          <Accordeon title="Impuestos Nacionales" data={nacionales} />
-        </Box>
-        <Box sx={{ width: '100%' }}>
-          <Accordeon title="Impuestos Departamentales" data={departamentales} />
-        </Box>
-        <Box sx={{ width: '100%' }}>
-          <Accordeon title="Impuestos Municipales" data={municipales} />
-        </Box>
+        {
+          loading ? <Spinner /> :
+            <div>
+              <Box sx={{ width: '100%' }}>
+                <Accordeon title="Impuestos Nacionales" data={nacionales} />
+              </Box>
+              <Box sx={{ width: '100%' }}>
+                <Accordeon title="Impuestos Departamentales" data={departamentales} />
+              </Box>
+              <Box sx={{ width: '100%' }}>
+                <Accordeon title="Impuestos Municipales" data={municipales} />
+              </Box>
+            </div>
+        }
+
       </Box>
     </Layout>
   )
-}
-
-export async function getServerSideProps(ctx: any) {
-  const url = `${process.env.API_URL}/impuesto`;
-
-  let taxes = [];
-  try {
-    const request = await axios.get(url);
-
-    taxes = request.data.data;
-  } catch (error) {
-    console.error(error);
-  }
-
-  return {
-    props: {
-      taxes
-    }
-  }
 }
 
 export default Home

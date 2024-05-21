e8b5b7027eda78c2367f67e9e7aec390e552f0b4
Luis Evilla <luisevilla588@gmail.com>
2024-05-21 00:30:19 -0500

    fix

--- controllers/departamento.tsx
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export async function getDepartamentos() {
-    const departamentos = await prisma.departamentos.findMany();
-
-    return departamentos;
-}

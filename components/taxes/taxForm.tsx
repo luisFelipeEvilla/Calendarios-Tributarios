6c9c177ef555a20477a9bd63cec0a255a1a45b98
Luis Felipe Evilla Rodriguez <luisevilla588@gmail.com>
2025-03-25 16:11:16 -0500

    agregar vigencias

--- components/taxes/taxForm.tsx
+++ components/taxes/taxForm.tsx
@@ -101,6 +101,12 @@ export default function TaxForm({ ...props }: PropsType) {
                 >
                     <MenuItem value={2023}>2023</MenuItem>
                     <MenuItem value={2024}>2024</MenuItem>
+                    <MenuItem value={2025}>2025</MenuItem>
+                    <MenuItem value={2026}>2026</MenuItem>
+                    <MenuItem value={2027}>2027</MenuItem>
+                    <MenuItem value={2028}>2028</MenuItem>
+                    <MenuItem value={2029}>2029</MenuItem>
+                    <MenuItem value={2030}>2030</MenuItem>
                 </Select>
             </FormControl>
             <FormControl fullWidth>

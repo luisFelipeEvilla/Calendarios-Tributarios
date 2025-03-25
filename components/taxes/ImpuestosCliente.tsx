a507c8a3c09a3b50c53e8a62d7a5850b70cc3b30
Luis Felipe Evilla Rodriguez <luisevilla588@gmail.com>
2025-03-25 16:25:08 -0500

    fix year

--- components/taxes/ImpuestosCliente.tsx
+++ components/taxes/ImpuestosCliente.tsx
@@ -75,7 +75,7 @@ export default function ImpuestosCliente({ ...props }) {
                     </TableHead>
                     <TableBody>
                         {
-                            props.impuestosCliente.map((tax: any, index: number) => {
+                            props.impuestosCliente.filter((tax: any) => tax.vigencia == year).map((tax: any, index: number) => {
                                 return (
                                     <TableRow key={index}>
                                         <TableCell>{tax.nombre}</TableCell>

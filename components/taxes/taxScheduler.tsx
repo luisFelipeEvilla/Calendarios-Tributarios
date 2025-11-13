228a8e7ba8c0780702178681f333edaf0abf2cb0
Luis Felipe Evilla Rodriguez <luisevilla588@gmail.com>
2025-11-13 16:10:50 -0500

    Update dependencies and refactor TaxScheduler component; removed Prisma schema

--- components/taxes/taxScheduler.tsx
+++ components/taxes/taxScheduler.tsx
@@ -1,41 +1,35 @@
-import { Scheduler, useScheduler } from "@aldabil/react-scheduler";
+import { Scheduler } from "@aldabil/react-scheduler";
 import { ProcessedEvent } from "@aldabil/react-scheduler/types";
 import { Box } from "@mui/material";
 import { es } from "date-fns/locale";
-import { useEffect } from "react";
+import { useMemo } from "react";
 import { cuota } from "../../types";
 
 type PropsType = { cuotas: cuota[] }
 
 export default function TaxScheduler({ ...props }: PropsType) {
-    const { setEvents } = useScheduler();
-
-    useEffect(() => {
-        const updateSchedule = () => {
-            const events: ProcessedEvent[] = [];
-            props.cuotas.forEach((feed, index) => {
-                feed.fechas_presentacion.forEach((fecha: any) => {
-                    if (typeof fecha.fecha === 'string') fecha.fecha = new Date(fecha.fecha);
-                    events.push({
-                        event_id: index,
-                        title: `${fecha.nit}`,
-                        start: fecha.fecha,
-                        end: fecha.fecha,
-                        color: '#3f51b5',
-                        textColor: 'white',
-                        allDay: true,
-                    })
+    const events: ProcessedEvent[] = useMemo(() => {
+        const eventsList: ProcessedEvent[] = [];
+        props.cuotas.forEach((feed, index) => {
+            feed.fechas_presentacion.forEach((fecha: any) => {
+                const fechaDate = typeof fecha.fecha === 'string' ? new Date(fecha.fecha) : fecha.fecha;
+                eventsList.push({
+                    event_id: `${index}-${fecha.nit}`,
+                    title: `${fecha.nit}`,
+                    start: fechaDate,
+                    end: fechaDate,
+                    color: '#3f51b5',
+                    textColor: 'white',
+                    allDay: true,
                 })
             })
-
-            setEvents(events);
-        }
-        updateSchedule();
+        })
+        return eventsList;
     }, [props.cuotas])
 
     return (
         <Box width='80%' marginBottom={10}>
-            <Scheduler locale={es} view='month' events={[]} editable={false} deletable={false} draggable={false} />
+            <Scheduler locale={es} view='month' events={events} editable={false} deletable={false} draggable={false} />
         </Box>
     )
 }

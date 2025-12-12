fb7ef94faf23fc478fa6fbab509d80aee2418076
Luis Felipe Evilla Rodriguez <luisevilla588@gmail.com>
2025-12-12 09:52:38 -0500

    Refactor Home component structure and update import statements
    - Changed layout from `Layout` component to a simple `div`.
    - Updated import statements to use consistent quotation marks.
    - Cleaned up code formatting for improved readability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,23 @@
-'use client'
-import { Box, Typography } from '@mui/material';
-import type { NextPage } from 'next';
-import Head from 'next/head';
+"use client";
+import { Box, Typography } from "@mui/material";
+import type { NextPage } from "next";
+import Head from "next/head";
 
 // import data
 
-// components
-import Layout from '../components/layout';
-import Accordeon from '../components/layouts/accordion';
+// components Home
 
 const Home: NextPage = () => {
   return (
-    <Layout>
+    <div>
       <Head>
         <title>Home</title>
       </Head>
-      <Box sx={{ width: '100%' }}>
-        <Typography variant='h2'>Bienvenido a SGC</Typography>
+      <Box sx={{ width: "100%" }}>
+        <Typography variant="h2">Bienvenido a SGC</Typography>
       </Box>
-    </Layout>
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
